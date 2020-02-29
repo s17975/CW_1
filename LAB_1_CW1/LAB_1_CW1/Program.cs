ec70f4e9d3494af15fd7b7f67982fe5ff103e7a7
Marcin Rudko <s17975@pjwstk.edu.pl>
2020-02-29 20:51:04 +0100

    Pierwszy commit

--- LAB_1_CW1/LAB_1_CW1/Program.cs
+++ LAB_1_CW1/LAB_1_CW1/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace LAB_1_CW1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-           
+            Console.WriteLine("Bardzo prosty crawler...");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
         }
     }
 }
