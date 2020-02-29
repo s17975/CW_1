4735d9588ab7f94e2e0ea7a541c5c36781bca931
s17975@pjwstk.edu.pl <s17975@pjwstk.edu.pl>
2020-02-29 21:22:58 +0100

    Finalna wersja crawlera

--- LAB_1_CW1/LAB_1_CW1/Program.cs
+++ LAB_1_CW1/LAB_1_CW1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LAB_1_CW1
@@ -12,17 +13,24 @@ namespace LAB_1_CW1
             //Java - Future
             //Task -> async await
 
-            Console.WriteLine("Bardzo prosty crawler...");
+            Console.WriteLine("Bardzo prosty crawler..."+"\n");
 
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
             var client = new HttpClient();
-            var response = await client.GetAsync("https://www.pja.edu.pl");
+            var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 string html = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(html);
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+                
             }
 
-            Console.WriteLine("Strona została zhakowana !");
+            Console.WriteLine("\n"+"Strona została zhakowana !");
         }
     }
 }
