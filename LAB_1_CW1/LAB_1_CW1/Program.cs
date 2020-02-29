9e30bb213308522c83f0e2e6525d645973843ac8
Marcin Rudko <s17975@pjwstk.edu.pl>
2020-02-29 20:39:37 +0100

    test git

---
+++ LAB_1_CW1/LAB_1_CW1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LAB_1_CW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+        }
+    }
+}
